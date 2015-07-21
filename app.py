2187cb3abee44d9a36967b71a88b0e4b72ea1621
Lawrence <lawrence@betterlife.io>
2015-07-22 00:58:40 +0800

    Add link to UI demo, change picture dirs.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!, <a href='static/index.html'>click here</a> for UI demo"
 
 if __name__ == "__main__":
     app.run()
