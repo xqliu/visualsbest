e75e1227d2754ae663ba23e9310a1fa935d0b3cf
Lawrence <lawrence@betterlife.io>
2015-07-28 06:28:58 +0800

    增加单元测试运行的各种工具和依赖，增加App信息注入，增加数据库表结构迁移工具

---
+++ run_tests.sh
@@ -0,0 +1,7 @@
+# Please make sure environment variable COVERALLS_REPO_TOKEN exists and is correct.
+# Reference: http://levibostian.com/python-code-coverage-and-coveralls-io/
+pip install -r test_requirements.txt
+mkdir -p reports/coverage_html
+nosetests -w test --with-coverage --cover-erase
+coverage html -d reports/coverage_html
+# coveralls
