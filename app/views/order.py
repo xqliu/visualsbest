e4f0be89b20a2453d319b6c6f15a5ec8ae484798
Lawrence <lawrence@betterlife.io>
2015-08-01 13:29:27 +0800

    增加订单和拍摄请求的管理界面(初步)

---
+++ app/views/order.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from app.views.base_view import ModelViewWithAccess
+from flask.ext.admin.model import InlineFormAdmin
+from flask.ext.admin.contrib.sqla import ModelView
+from app.models.order import OrderComment
+from flask.ext.babelex import lazy_gettext
+
+class OrderCommentsInlineAdmin(InlineFormAdmin):
+    form_args = dict(
+        comment=dict(label=u'关联评论'),
+        rating=dict(label=u'评分')
+    )
+
+class OrderAdmin(ModelViewWithAccess):
+    inline_models = (OrderCommentsInlineAdmin(OrderComment),)
+
+    form_columns = ('request', 'status', 'order_comments')
+
+    column_labels = {
+        'request' : u'关联拍摄请求',
+        'status': u'订单状态',
+        'order_comments': u'订单评论',
+    }
