7a79bfa6f36865b6172a7bf7bd9f3c6e6d6b0e9a
Lawrence <lawrence@betterlife.io>
2015-08-13 23:23:45 +0800

    继续解决自动升级数据库表结构时的锁

--- migrations/versions/06_33eb76c3c32f_.py
+++ migrations/versions/06_33eb76c3c32f_.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-"""在作品集和作品分类对象中增加名字、分类、风格、日期等
+"""在作品集中增加名字、分类、风格、日期、简介等，作品分类中增加名称
 
 Revision ID: 33eb76c3c32f
 Revises: 4b28ce000424
@@ -21,7 +21,8 @@ def upgrade():
         'name', sa.String(length=32), nullable=False))
     op.add_column('photo_collection', sa.Column(
         'category_id', sa.Integer(), nullable=True))
-    op.create_foreign_key(None, 'photo_collection', 'photo_category',
+    op.create_foreign_key(u'photo_collection_category_id_fkey',
+                          'photo_collection', 'photo_category',
                           ['category_id'], ['id'])
     op.add_column('photo_collection', sa.Column(
         'introduce', sa.String(length=256), nullable=True))
